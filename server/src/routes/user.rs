d316c8d6a92e71a03c26cc98dd511411c9b0fba3
ad-tra <you@example.com>
2022-09-05 21:03:15 -0400

    merge from new-server

---
+++ server/src/routes/user.rs
@@ -0,0 +1,99 @@
+use crate::error::ServerError;
+use crate::error::ServerError::Expected;
+use crate::user_api::{CreateAccountError, CreateAccountRequest, LoginError, LoginRequest};
+use crate::{db, Db};
+use rand::Rng;
+use rocket::serde::json::Json;
+use rocket_db_pools::Connection;
+use sqlx::types::Uuid;
+use sqlx::Acquire;
+
+#[post("/create-account", data = "<request>")]
+pub async fn create_account(
+    mut db: Connection<Db>,
+    request: Json<CreateAccountRequest>,
+) -> Result<(), ServerError<CreateAccountError>> {
+    let mut transaction = (&mut *db).begin().await?;
+
+    if request.graduation_year < 2022 || request.graduation_year > 2026 {
+        return Err(Expected(CreateAccountError::InvalidGraduationYear(
+            "Invalid graduation year.".to_string(),
+        )));
+    }
+
+    if !is_password_valid(&request.password) {
+        return Err(Expected(CreateAccountError::InvalidPassword(
+            "Your password is invalid.".to_string(),
+        )));
+    }
+
+    if db::does_email_exist(&mut transaction, &request.email).await? {
+        return Err(Expected(CreateAccountError::EmailExists(
+            "This email already exists.".to_string(),
+        )));
+    }
+
+    if !check_if_email_exists::syntax::check_syntax(&request.email).is_valid_syntax {
+        return Err(Expected(CreateAccountError::BadEmail(
+            "Invalid email.".to_string(),
+        )));
+    }
+
+    let salt: [u8; 10] = rand::thread_rng().gen();
+    let password_hash = argon2::hash_encoded(
+        request.password.as_bytes(),
+        &salt,
+        &argon2::Config::default(),
+    )
+    .unwrap();
+
+    db::add_account(
+        &mut transaction,
+        &request.username,
+        &request.email,
+        &password_hash,
+        &request.first_name,
+        &request.last_name,
+        &request.gender,
+        request.graduation_year,
+    )
+    .await?;
+
+    transaction.commit().await?;
+
+    Ok(())
+}
+
+fn is_password_valid(password: &str) -> bool {
+    password.len() > 7
+        && password
+            .chars()
+            .any(|char| char == '&' || char == '!' || char == '$' || char == '@' || char == '*')
+        && password.chars().any(char::is_uppercase)
+        && password.chars().any(char::is_numeric)
+}
+
+#[post("/login", data = "<request>")]
+pub async fn login(
+    mut db: Connection<Db>,
+    request: Json<LoginRequest>,
+) -> Result<String, ServerError<LoginError>> {
+    let mut transaction = (&mut *db).begin().await?;
+
+    let password_hash = db::get_password_hash(&mut transaction, &request.username).await?.ok_or(Expected(LoginError::UsernameDoesNotExist(
+        "This username does not exist.".to_string(),
+    )))?;
+
+    if argon2::verify_encoded(&password_hash, request.password.as_bytes()).unwrap() {
+        let session_id = Uuid::new_v4();
+        db::set_session_id(&mut transaction, &request.username, session_id).await?;
+
+        transaction.commit().await?;
+
+        Ok(session_id.to_string())
+    } else {
+        Err(Expected(LoginError::IncorrectPassword(
+            "Your password is incorrect.".to_string(),
+        )))
+    }
+}
---
+++ server/src/user_api.rs
@@ -0,0 +1,87 @@
+use crate::{db, Db};
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome};
+use rocket::serde::{Deserialize, Serialize};
+use rocket::Request;
+use rocket_db_pools::Connection;
+use std::str::FromStr;
+use uuid::Uuid;
+
+pub struct AuthorizedUser {
+    pub username: String,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for AuthorizedUser {
+    type Error = ();
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, ()> {
+        let session_id = match request.headers().get_one("Session-Id") {
+            None => return Outcome::Failure((Status::Unauthorized, ())),
+            Some(str_session_id) => match Uuid::from_str(str_session_id) {
+                Ok(session_id) => session_id,
+                Err(_) => return Outcome::Failure((Status::Unauthorized, ())),
+            },
+        };
+
+        let conn = request.guard::<Connection<Db>>().await.unwrap();
+
+        let username = match db::get_username(conn, session_id).await {
+            Ok(Some(username)) => username,
+            _ => return Outcome::Failure((Status::Unauthorized, ())),
+        };
+
+        Outcome::Success(AuthorizedUser { username })
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+#[derive(sqlx::Type)]
+#[sqlx(rename_all = "lowercase")]
+pub enum Gender {
+    Male,
+    Female,
+    Other,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+#[serde(rename_all = "camelCase")]
+pub struct CreateAccountRequest {
+    pub email: String,
+    pub username: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub password: String,
+    pub gender: Gender,
+    pub graduation_year: i16,
+}
+
+#[derive(Responder)]
+pub enum CreateAccountError {
+    #[response(status = 400)]
+    InvalidPassword(String),
+    #[response(status = 409)]
+    EmailExists(String),
+    #[response(status = 400)]
+    BadEmail(String),
+    #[response(status = 400)]
+    InvalidGraduationYear(String),
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+#[serde(rename_all = "camelCase")]
+pub struct LoginRequest {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Responder)]
+pub enum LoginError {
+    #[response(status = 400)]
+    IncorrectPassword(String),
+    #[response(status = 409)]
+    UsernameDoesNotExist(String),
+}
